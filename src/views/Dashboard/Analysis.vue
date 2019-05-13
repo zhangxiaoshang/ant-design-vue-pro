bbbdb47c3e210fb1154f2638cb0743706e4ad846
zhangxiaoshang <zhangxiaoshang66@163.com>
2019-05-13 21:23:56 +0800

    如何在组件中使用echart

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>分析页</div>
+  <div>
+    <Chart :option="chartOption" style="height: 400px">/</Chart>
+  </div>
 </template>
 
 <script>
-export default {};
+import Chart from "@/components/Chart";
+import random from "lodash/random";
+export default {
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+      );
+      this.chartOption = { ...this.chartOption };
+    }, 3000);
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  }
+};
 </script>
 
 <style></style>
